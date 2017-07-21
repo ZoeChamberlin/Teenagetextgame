58589a214fe38a50281c6afe7fdfd39974087278
ZoeChamberlin <zoeswimmer4@gmail.com>
2017-07-21 11:43:10 -0400

    add

---
+++ Teenagelife.py
@@ -0,0 +1,49 @@
+print("You're a teenager living in your teenage world. You're constantly experiencing new things and overcoming obsatcles. Making the right decisions can lead to different futures and help determine your destiny ")
+print("Make the right decisions and your destiny will be revealed..")
+print(" You can are a teenage girl")
+print("You just started your senior year of high school!! You're super excited graudate and stay lit all year long!! You walk past a club fair on you way through the hall.")
+answer1 = input(" Do you stop and join a club?")
+answer= "answer"
+yes ="yes"
+no = "no"
+if answer=="yes":
+    print("You find a bunch of clubs you're interested in, but now you're gonna be late to class! Now you have to rush to your next class which is all the way across the school!")
+if answer == "no":
+    print("You go to free and realize none of your friends have the same free as you! You realize you wasted your team and should have gone to the club fair.")
+print(" You go to class and see your crush! You start talking and they invite you to come to a house party Friday Night. You would love to go, but you have your SAT Saturday morning! ")
+answer1= input(" Do you go to the party?")
+if answer == "yes":
+      print("You go to the party and have a great time, but when you wake up Saturday morning you would rather go back to sleep then take a 3 hour test. You get your results back and your score goes down 200 points!!.")
+if answer == "no":
+      print("You wake the morning of the SAT and feel refreshed and when you get your results back your score increased by 300 points!!")
+      print("College application time is rolling around and you're FREAKING out because you're afriad you won't get into your dream school. Your friends suggest you go to a yoga class with them.")
+answer1 = imput(" Do you go to the yoga class?")
+if answer =="yes" :
+        print("Your stress decreases and you realize whatever happens in the college process happens for a reason. You relax with your friends and enjoy your day! You get home and your parents start bugging you about where you were all day. You have the urge to yell and scream.")
+if answer == "no":
+        print("You stay home and break out into a stress rash. Your parents don't know what to do so they call an ambulance. You stay in the hospital for a week and cost your parents a lot of money.")
+    answer1= input(" Do you yell and scream at them?")
+if answer == "yes" :
+        print(" Your parents ground you and you can't relax with you friends for the next month!")
+if answer == "no" :
+            print( "You keep your grownd and read a book to calm down")
+    print("It's March and your college acceptance letters are coming out! You want to open your letters with friends so you go to your girl Chloe's house! You open the letters together and Julia gets into your dream school and you don't.")
+answer1= input(" Do you congratulate your friend even though you're upset?")
+if answer == "yes"
+        print(" You celebrate with Chloe and realize that everything happens for a reason")
+    if answer == "no"
+        print(" You cry and go home and stop talking to Chloe for a week. This severely damages your friendship! #shameonyou")
+print("Graduation day is approaching!! There are lots of parties being thrown even though it's finals week! Your best friend is throwing a party the nught before your biochemistry final. Yikes!!")
+answer1 = input("Do you go to the party?")
+if answer ==  "yes"
+    print("You have a great time at the party, but you fail your biochem final the next morning. ")
+if answer == "no"
+    print("You review topics for your final in the morning and feel confident that you got a 90 or above!")
+    print("It's graduation day and you're so glad you're done with high school! You can't wait to start college! Your family wants to take you out to dinner, but once again your friend invites you somehwere.")
+    answer1 = input("Do you hang out with your friend?")
+if answer == "yes"
+    print(" You hang out with your friend, but can't help feeling guilty about not spending time with your family..you regret not going out to dinner with them")
+    print("* fast forward 20 years* You're done with college, but you're having trouble finding a stable job due to your problems with time management and inability to get your priorities straight :)")
+if answer == "no"
+    print(" You're glad you went out to dinner with your family and you know you're going to miss them while you're away. * fast forward 20 years* You have a stable job, boyfriend, and make over 100K. You're living your best life! yaaaassss.")
+    
